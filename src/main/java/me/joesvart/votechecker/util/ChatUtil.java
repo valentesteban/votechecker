2830abe773d254139a7c704f7abf7047a8e1e769
Joesvart <63020706+joesvart@users.noreply.github.com>
2022-02-11 21:25:27 -0300

    refactor: remove not used translates

--- src/main/java/me/joesvart/votechecker/util/ChatUtil.java
+++ src/main/java/me/joesvart/votechecker/util/ChatUtil.java
@@ -7,47 +7,7 @@ import java.util.List;
 
 public class ChatUtil {
 
-    /**
-     * Translate string.
-     *
-     * @param in the in
-     * @return the string
-     */
-    public static String translate(String in) {
-        return ChatColor.translateAlternateColorCodes('&', in);
-    }
-
-    /**
-     * Translate list.
-     *
-     * @param lines the lines
-     * @return the list
-     */
-    public static List<String> translate(List<String> lines) {
-        List<String> toReturn = new ArrayList<>();
-
-        for (String line : lines) {
-            toReturn.add(ChatColor.translateAlternateColorCodes('&', line));
-        }
-
-        return toReturn;
-    }
-
-    /**
-     * Translate list.
-     *
-     * @param lines the lines
-     * @return the list
-     */
-    public static List<String> translate(String[] lines) {
-        List<String> toReturn = new ArrayList<>();
-
-        for (String line : lines) {
-            if (line != null) {
-                toReturn.add(ChatColor.translateAlternateColorCodes('&', line));
-            }
-        }
-
-        return toReturn;
+    public static String translate(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
