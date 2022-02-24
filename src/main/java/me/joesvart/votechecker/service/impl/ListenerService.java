ac4df446ae702cfb3a68d4311852114170424e3f
Joesvart <63020706+joesvart@users.noreply.github.com>
2022-02-23 21:02:44 -0300

    refactor: remove the old method, no need to use that with only one listener

--- src/main/java/me/joesvart/votechecker/service/impl/ListenerService.java
+++ src/main/java/me/joesvart/votechecker/service/impl/ListenerService.java
@@ -4,8 +4,6 @@ import me.joesvart.votechecker.VoteChecker;
 import me.joesvart.votechecker.listener.PlayerJoinListener;
 import me.joesvart.votechecker.service.Service;
 import me.yushust.inject.InjectAll;
-import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 
 @InjectAll
 public class ListenerService
@@ -17,14 +15,10 @@ public class ListenerService
 
     @Override
     public void start() {
-        registerListeners(
-                playerJoinListener
-        );
-    }
-
-    private void registerListeners(Listener... listeners) {
-        for (Listener listener : listeners) {
-            Bukkit.getPluginManager().registerEvents(listener, voteChecker);
-        }
+        // used this method cause is only one listener
+        voteChecker
+                .getServer()
+                .getPluginManager()
+                .registerEvents(playerJoinListener, voteChecker);
     }
 }
